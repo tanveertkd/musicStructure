2b925f32e88ba8bdba4da1445e65c78b2b7784e5
tanveertkd <hazarikatanveer12@gmail.com>
2018-07-25 15:14:08 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/baseplate/coldplayer/navigation.java
@@ -0,0 +1,60 @@
+package com.example.baseplate.coldplayer;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class navigation extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        setToolbar();
+        navigationView = (NavigationView) findViewById(R.id.navigationMenu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.albums:
+                        Toast.makeText(navigation.this, "Albums", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.playlist:
+                        break;
+                    case R.id.favourites:
+                        break;
+                    case R.id.mostPlayed:
+                        break;
+                    case R.id.news:
+                        break;
+                    case R.id.stream:
+                        break;
+                    case R.id.settings:
+                        break;
+
+                }
+                return false;
+            }
+        });
+    }
+
+    public void setToolbar(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+}
